511ad8b7097e6c5d8c4689842f2df97a0f47786d
Eduardo Muchak <eduardomuchak@gmail.com>
2022-09-15 18:34:57 -0300

    feat(req4): :sparkles: create the error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error.middleware';
 
 const app = express();
+
+app.use(errorHandler);
 
 export default app;
