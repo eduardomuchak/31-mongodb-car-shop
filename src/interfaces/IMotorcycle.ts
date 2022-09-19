fd2a73236f11f510006abbde94ebf45f112bf59a
Eduardo Muchak <eduardomuchak@gmail.com>
2022-09-19 19:34:36 -0300

    fix: :art: adjust motorcycle create validation

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -3,7 +3,7 @@ import { VehicleZodSchema } from './IVehicle';
 
 const MotorcycleZodSchema = VehicleZodSchema.extend({
   category: z.enum(['Street', 'Custom', 'Trail']),
-  engineCapacity: z.number().int().lte(2500),
+  engineCapacity: z.number().int().positive().lte(2500),
 });
 
 type IMotorcycle = z.infer<typeof MotorcycleZodSchema>;
